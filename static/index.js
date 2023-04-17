4dc2878ea7c2ccfcb539793a0e1887eb49df83b2
andrew <henry.medina@tecsup.edu.pe>
2023-04-17 18:19:50 -0500

    other commit

---
+++ static/index.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelector("#submit").disabled = true;
+});
+
+function validate() {
+	document.querySelector("#submit").disabled  = false;
+
+	document.querySelectorAll('.check_empty').forEach(input => {
+		if(input.value.length == 0 ){
+			document.querySelector("#submit").disabled =  true;
+		}
+	});
+}
